22279cc21161d0b96e3883c39e9bfc852ea7557a
Kevin DeMers <kevin.demers@demerske.com>
2016-04-18 18:39:28 -0600

    no message

---
+++ Clean_Empty_Folders.ps1
@@ -0,0 +1,19 @@
+﻿$path = 'X:\Music'
+
+$folders = @()
+
+ForEach($folder in (gci $path -recurse | ?{$_.psiscontainer})){
+    $folders += new-object PSObject -property @{
+        Object = $folder
+        Depth = ($folder.fullname.split("\")).count
+    }
+}
+
+$folders = $folders | Sort Depth -Descending
+
+ForEach($folder in $folders){
+    If($folder.object.getfilesysteminfos().count -eq 0){
+        remove-item $folder.object.fullname -force
+        Write-Host $folder.object.fullname
+    }
+}
