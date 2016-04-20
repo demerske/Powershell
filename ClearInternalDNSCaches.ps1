cd663fcaca94199ae043a917a3f2d17faac66273
Kevin DeMers <kevin.demers@demerske.com>
2016-04-19 20:21:53 -0600

    no message

---
+++ ClearInternalDNSCaches.ps1
@@ -0,0 +1,20 @@
+﻿$sroots = @(#array of LDAP connections for domains to search#)
+$error.clear()
+
+$find = new-object system.directoryservices.directorysearcher
+$find.filter = "(&(objectcategory=computer)(operatingsystem=*Server*))"
+
+ForEach($sroot in $sroots){
+    $find.searchroot = $sroot
+
+    $list = $find.findall()
+
+    ForEach($itm in $list){
+        $comp = [string]$itm.properties.name
+        $cache = Gwmi -computer $comp -namespace "root\MicrosoftDNS" -Class "MicrosoftDNS_Cache"
+        IF($error[0] -ne $null){
+            Write-Host $comp, $error[0].exception.message.tostring()
+            $error.clear()}
+        Else{$cache.clearcache()}
+    }
+}
---
+++ DiskCleaner.ps1
@@ -0,0 +1,44 @@
+﻿$os = gwmi win32_operatingsystem | Select caption
+
+$safe = @('Administrator','Default','Default user','Public')
+$upaths03 = @('\local settings\temp','\local settings\temporary internet files','\cookies')
+
+$cleanpaths = @('c:\temp','c:\windows\temp','c:\recycler')
+
+If($os.caption.contains('2008')){
+    $ufold = gci 'c:\users'
+    ForEach($uf in $ufold){
+        If($safe -notcontains $uf.name){
+            $temp = $uf.fullname + "\appdata\local\temp"
+            gci $temp | ForEach-Object{ri $_.fullname -recurse -force -confirm:$false -erroraction silentlycontinue}
+        }
+    }
+}
+
+If($os.caption.contains('2003')){
+    $ufold = gci 'c:\documents and settings'
+    ForEach($uf in $ufold){
+        If($safe -notcontains $uf.name){
+            ForEach($suf in $upaths03){
+                $path = $uf.fullname + $suf
+                gci $path | foreach-object{ri $_.fullname -recurse -force -confirm:$false -erroraction silentlycontinue}
+            }
+        }
+    }
+}
+
+ForEach($a in $cleanpaths){
+    $subs = gci $a
+    $subs | ForEach-Object{ri $_.fullname -recurse -force -confirm:$false -erroraction silentlycontinue}
+}
+
+If($os.caption.contains('2003'){
+    $oldpatches = gci $env:windir | ?{$_.name.contains('ntuninstall')}
+    $date = get-date
+    ForEach($b in $oldpatches){
+        $age = new-timespan $b.lastwritetime $date
+        If($age.days -gt 90){
+            ri $b.fullname -recurse -force -confirm:$false -erroraction silentlycontinue
+        }
+    }
+}
